2e63f2f1674b8009e6c007d6c857ffa2289b6f4f
Dmitry <matrix.predictor@gmail.com>
2015-10-13 21:07:44 +0500

    Добавляет несколько глав в часть по вебу

--- sourse/main.js
+++ sourse/main.js
@@ -2,10 +2,8 @@ $( document ).ready(function() {
    $(".cs-head").on( "click", function() {
       var id = $(this)[0].id;
       paragraphHeader = $('#'+id.slice(2) + ' .paragraph-header').html();
-      paragraphSubheader = $('#'+id.slice(2) + ' .paragraph-subheader').html();
       paragraphBody = $('#'+id.slice(2) + ' .paragraph-body').html();
       $("#paragraph-title").text(paragraphHeader);
-      $("#paragraph-subtitle").text(paragraphSubheader);
       $("#paragraph-content").html(paragraphBody);
    });
    $("#p-home").click();
