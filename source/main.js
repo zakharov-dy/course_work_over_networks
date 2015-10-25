fda5d8e3e52eecf851a75547ab4f27cccc8b6542
Dmitry <matrix.predictor@gmail.com>
2015-10-25 23:11:50 +0500

    1. Добавляет кнопку для поднятия наверх 2. Добавляет два раздела - веб-прокси и серверные веб-фермы

--- source/main.js
+++ source/main.js
@@ -1,4 +1,17 @@
+var show = 150;
+var delay = 1000;
 $( document ).ready(function() {
+
+   $(window).scroll(function() {
+      if ($(this).scrollTop() > show) $('#cs-footer').fadeIn();
+      else $('#cs-footer').fadeOut();
+   });
+   $('#cs-footer').click(function () {
+      $('body, html').animate({
+         scrollTop: 0
+      }, delay);
+   });
+
    $(".cs-head").on( "click", function() {
       var id = $(this)[0].id;
       paragraphHeader = $('#'+id.slice(2) + ' .paragraph-header').html();
